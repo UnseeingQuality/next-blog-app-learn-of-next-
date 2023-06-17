3fd9650a9b4b80c6aecda6e27db05e5e43ef24e5
UnseeingQuality <balakinaa2016@mail.ru>
2023-06-17 17:33:34 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About | Next app",
+}
+
+
+export default function About() {
+    return <h3>Select subitem</h3>;
+}
---
+++ components/TheFooter.tsx
@@ -0,0 +1,8 @@
+const TheFooter = () => {
+    return (
+        <footer className="container">
+            Created by &copy;Unseeing-Quality
+        </footer>
+    )
+}
+export {TheFooter};
